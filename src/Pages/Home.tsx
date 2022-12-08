a3c4e6f33e62bef5c8820d514bb3fc7c66097620
YWQ1998 <3511350954@qq.com>
2022-12-08 10:36:47 +0800

    [MILESTONE] C7N UI 安装及配置完成

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,13 +3,18 @@
  * @Author: YinWQ
  * @Date: 2022-12-08 09:50:02
  * @LastEditors: YinWQ
- * @LastEditTime: 2022-12-08 09:50:02
+ * @LastEditTime: 2022-12-08 10:32:55
  */
 import React from 'react'
+import {Button} from 'choerodon-ui/pro'
+import { ButtonColor } from 'choerodon-ui/pro/lib/button/enum'
 
 const Home:React.FC = () => {
   return (
-    <div>Home</div>
-  )
+    <div>
+      Home
+      <Button color={ButtonColor.red}>Button</Button>
+    </div>
+  );
 }
 export default Home;
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { overwriteDefaultConfig } from "choerodon-ui/dataset";
+import defaults from "choerodon-ui/lib/configure/default";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,3 +14,4 @@ root.render(
       <App />
   </React.StrictMode>
 );
+overwriteDefaultConfig(defaults);
