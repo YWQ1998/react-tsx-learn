a127f8f627092728c7f9e19f13cefdfcdfd752c3
YWQ1998 <3511350954@qq.com>
2022-12-08 14:42:22 +0800

    [MILE] less 样式引入

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,16 +3,16 @@
  * @Author: YinWQ
  * @Date: 2022-12-08 09:50:02
  * @LastEditors: YinWQ
- * @LastEditTime: 2022-12-08 10:32:55
+ * @LastEditTime: 2022-12-08 14:35:15
  */
 import React from 'react'
 import {Button} from 'choerodon-ui/pro'
 import { ButtonColor } from 'choerodon-ui/pro/lib/button/enum'
-
+import './home.less'
 const Home:React.FC = () => {
   return (
     <div>
-      Home
+      <span className='title'>Home</span>
       <Button color={ButtonColor.red}>Button</Button>
     </div>
   );
